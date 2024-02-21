793fcc84e47fa5a887a124ff14c352803e218d65
andrea-livornese <andrealivornese@gmail.com>
2024-02-21 15:45:51 +0100

    stampa delle email in pagina

--- js/script.js
+++ js/script.js
@@ -3,10 +3,22 @@
 
 const { createApp } = Vue
 
-  createApp({
+createApp({
     data() {
-      return {
-        
-      }
+        return {
+
+            emails:[],
+        }
+    },
+    mounted() {
+
+
+        for(let i =0; i<10;i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then( (result)=>{
+                this.emails.push(result.data.response);
+            });
+        }
+
+
     }
-  }).mount('#app')
+}).mount('#app')
